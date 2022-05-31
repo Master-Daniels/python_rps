74bfd1f9c96cbd81beb077a7fe0e19e35134410a
Master-Daniels <adebayooluwasegun011@gmail.com>
2022-05-31 14:22:40 +0100

    Finished working on the handling wrong user input.

--- main.py
+++ main.py
@@ -1,21 +1,23 @@
 import random
 
-computer_options = ["R", "P", "S"]
-computer_choice = random.choice(computer_options)
+choices = ["R", "P", "S"]
+computer_choice = random.choice(choices)
 choices_map = {
     "S": "Scissors",
     "P": "Paper",
     "R": "Rock"
 }
-
+print("""
+Welcome to this fun little Rock, Paper and Scissors
+game, instructions on how to play are in the README file.
+options are : 'R' for Rock, 'S' for Scissors and 'P' for Paper.
+""")
 while True:
-    user_choice = input("""
-Welcome to this fun Rock,Paper, Scissors game
-Options: "R" for Rock, "P" for Paper
-and "S" for Scissors \n
-Enter a choice: """).upper()
-    if user_choice == computer_choice:
-        result = "Tie"
+    user_choice = input("Enter choice: ").upper()
+    if user_choice not in choices:
+        print("You have made a wrong choice, try again")
+    elif user_choice == computer_choice:
+        result = "Tied"
         print(f"{result}, \nPlayer({choices_map[user_choice]}) : CPU({choices_map[computer_choice]})")
     elif (user_choice == "P" and computer_choice == "R") or (user_choice == "S" and computer_choice == "P") or (
             "R" == user_choice and computer_choice == "S"):
